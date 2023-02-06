78030fa4a0a4e1cbc7a78f19277e6ef122e4dbcb
Phil Howard <phil@pimoroni.com>
2023-02-06 14:31:30 +0000

    Inky: Add Inky 7.3 support.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,10 @@
 # Invoke plugins here to populate the cache
+python3 -m plugins.xkcd 800x480     # Inky 7.3"
 python3 -m plugins.xkcd 600x448     # Inky 5.7"
 python3 -m plugins.xkcd 640x400     # Inky 4.0"
+python3 -m plugins.jokeapi 800x480  # Inky 7.3"
 python3 -m plugins.jokeapi 600x448  # Inky 5.7"
 python3 -m plugins.jokeapi 640x400  # Inky 4.0"
+python3 -m plugins.nasa_apod 800x480 # Inky 7.3"
 python3 -m plugins.nasa_apod 600x448 # Inky 5.7"
 python3 -m plugins.nasa_apod 640x400 # Inky 4.0"
